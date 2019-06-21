c6f22770565e6f919d7ce8d695b113777c6f5282
kian0949 <kian0949@edu.campusvejle.dk>
2019-06-21 12:11:20 +0200

    Added unit tests to backend code

---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+using RideService.Entities;
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void DaysSinceLastShutdown_ReturnsCorrectDaysWithOneBrokenReport()
+        {
+            //Arrange
+            Ride
+
+            //Action
+
+
+            //Assert
+
+        }
+    }
+}
